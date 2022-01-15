bd060d1aa3943065ead4851b96065400e0cd398c
Tiara Brown <tiaradominique@gmail.com>
2022-01-15 11:30:44 -0500

    Still working on challenge 4

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -69,7 +69,11 @@ HR has asked you to change the data to make it easier to print so that it looks 
 ------------------------------------------------------------------------------------------------ */
 
 const updateNumbers = (obj) => {
-  // Solution code here...
+  let updatedKeys = Object.keys(obj);
+  let updatedValues = Object.values(obj);
+  updatedKeys.map(values => {
+
+  })
 };
 
 
