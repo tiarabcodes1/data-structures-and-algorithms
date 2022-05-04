431e60b12288f08768995d286c4f83f9e756460e
Tiara Brown <tiaradominique.b@gmail.com>
2022-05-04 01:32:04 -0400

    Completed Whiteboard for treeIntersection and setup files for testing.

---
+++ javascript/hashtable/tree-intersection/tree-intersection.js
@@ -0,0 +1,10 @@
+const hashMap = require('../repeatedWord.js');
+
+// Write a function called tree_intersection that takes two binary trees as parameters.
+//Using your Hashmap implementation as a part of your algorithm, return a set of values found in both trees.
+
+ cosnt treeIntersection = () => {
+
+}
+
+module.exports = { treeIntersection }
