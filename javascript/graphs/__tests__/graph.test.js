32b65976659775ba5120f1fc79d4d81f0f7f3882
tiarabcodes1 <tiaradominique.b@gmail.com>
2022-05-11 18:22:12 -0400

    Added DepthFirst method and testing functionality.

--- javascript/graphs/__tests__/graph.test.js
+++ javascript/graphs/__tests__/graph.test.js
@@ -30,10 +30,6 @@ describe('TESTING GRAPH METHODS', () => {
     expect(console.log(nodes)).toEqual(newGraph.vertex);
   });
 
-  test('All appropriate neighbors can be retrieved from the graph', () => {
-    
-  });
-
   test('Neighbors are returned with the weight between nodes included', () => {
     const newGraph = new Graph();
     const value100 = newGraph.addVertex('value100');
@@ -79,4 +75,29 @@ describe('TESTING GRAPH METHODS', () => {
     expect(console.log(results)).toEqual(newGraph.vertex);
 
   });
+
+  test('Should return A collection of nodes in their pre-order depth-first traversal order', () => {
+    const newGraph = new Graph;
+
+    const value1 = newGraph.addVertex('value1');
+    const value2 = newGraph.addVertex('value2');
+    const value3 = newGraph.addVertex('value3');
+    const value4 = newGraph.addVertex('value4');
+
+    newGraph.addDirectedEdge(value2, value1);
+    newGraph.addDirectedEdge(value1, value2);
+    newGraph.addDirectedEdge(value1, value3);
+    newGraph.addDirectedEdge(value1, value4);
+    newGraph.addDirectedEdge(value3, value1);
+    newGraph.addDirectedEdge(value3, value4);
+    newGraph.addDirectedEdge(value4, value3);
+    newGraph.addDirectedEdge(value4, value1);
+
+    let results = newGraph.depthFirst(value2);
+
+    expect(console.log('DepthFirst: ',results)).toEqual(newGraph.vertex);
+
+  });
+
+
 });
