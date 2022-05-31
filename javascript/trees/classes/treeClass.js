a9aa925263075f34c7d2d68109672aac3311923f
tiarabcodes1 <tiaradominique.b@gmail.com>
2022-05-31 09:22:55 -0400

    Practicing trees

--- javascript/trees/classes/treeClass.js
+++ javascript/trees/classes/treeClass.js
@@ -19,30 +19,17 @@ class BinaryTree {
     this.root = null;
   }
 
-  //Recursive approach
+  
   preOrder() {
-    //Traversed values of tree
     let results = [];
-
-    //Recursive helper function, walks through all values and does something with the node <-- currNode argument
-    let traverse = (node) => {
-
-      // Pushes the added value in depending on conditions
-      results.push(node.value);
-
-      // decides where added value belongs on subtrees
-      if (node.left) {
-        traverse(node.left);
+    const preOrderHelper = (root) => {
+      if(root !== null){
+        results.push(root.value);
+        preOrderHelper(root.left);
+        preOrderHelper(root.right);
       }
-      if (node.right) {
-        traverse(node.right);
-      }
-      // else {
-      //   throw new Error('Sorry value must not be equal to current nodes');
-      // }
     };
-    //runs the function starting at the root
-    traverse(this.root);
+    preOrderHelper(this.root);
     return results;
   }
 
@@ -120,8 +107,22 @@ class BinaryTree {
     return maxValue;
   }
 
+  
+
 }
 
+let tree = new BinaryTree();
+
+tree.root = new Node(9);
+tree.root.left = new Node(5);
+tree.root.left.left = new Node(3);
+tree.root.left.right = new Node(7);
+tree.root.left.right.left = new Node(6);
+tree.root.left.left.right = new Node(4);
+tree.root.right = new Node(11);
+tree.root.right.left = new Node(10);
+
+console.log(tree.inOrder());
 
 class BinarySearchTree extends BinaryTree {
 
@@ -180,64 +181,14 @@ class BinarySearchTree extends BinaryTree {
 }
 
 
-class KaryTree {
-  constructor(k) {
-    this.k = k;
-    this.root = null;
-  }
-}
-function breadthFirst(node){
-  const arr = [];
-  const result = [];
-  let current = node;
-
-  arr.unshift(node);
-
-  while(arr.length){
-    current = arr.pop();
-    result.push(current.value);
-    for (let node of current.children){
-      arr.unshift(node);
-    }
-  }
-  return result;
-}
-
-function fizzBuzz(node){
-  let result = [];
-  let i;
-
-  for(i = 0; i < node.length; i++) {
-    if(node[i] % 15 === 0) result.push("FizzBuzz");
-    else if(node[i] % 3 === 0) result.push("Fizz");
-    else if (node[i] % 5 === 0) result.push("Buzz");
-    else result.push(node[i].toString());
-  }
-  return result;
-}
 
 
-// let tree = new KaryTree(4);
 
-// tree.root = new Node(10);
-// tree.root.children.push(new Node(1), new Node(3), new Node(15), new Node(32));
-// tree.root.children[0].children.push( new Node(40), new Node(30), new Node(9));
-// tree.root.children[1].children.push( new Node(45), new Node(38), new Node(90));
-// tree.root.children[2].children.push( new Node(12), new Node(60), new Node(41));
-// tree.root.children[3].children.push( new Node(80), new Node(66), new Node(49));
-// // tree.root.left.left = new Node(1);
-// // tree.root.left.right = new Node(7);
-// // tree.root.right.left = new Node(30);
-// // tree.root.right.right = new Node(56);
-// // tree.root.left.right.right = new Node(9);
-// // console.log('B FIRST: ', breadthFirst(tree.root));
-// console.log('FIZZ BUZZ: ', fizzBuzz(breadthFirst(tree.root)));
-// // console.log('CHILDREN LOG: ', tree.root.children[3].children);
-// // console.log('FULL TREE: ',tree);
+
 
 module.exports = {
   BinaryTree,
   Node,
   BinarySearchTree,
-  KaryTree
+  // KaryTree
 };
